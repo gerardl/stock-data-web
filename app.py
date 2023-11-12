fa338641c724df28d9a6388720e2165f5dcf490f
Gerard Lucas <gerard.lucas@conperio.com>
2023-11-11 21:58:06 -0600

    form style

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from stock_loader import StockLoader
+from av_service import AlphaVantageService
+from chart_service import ChartService
 
 PORT = 5001
 
@@ -14,6 +16,19 @@ def load_stock_data():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        symbol = request.form['symbol']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        chart_type = int(request.form['chart_type'])
+        try:
+            av_service = AlphaVantageService('demo')
+            time_series = av_service.get_time_series_daily(symbol, start_date, end_date)
+            chart_service = ChartService()
+            chart_service.create_chart(chart_type, time_series)
+        except Exception as e:
+            flash(str(e))
+            return redirect(url_for('index'))
     return render_template('index.html', stocks=app.stocks)
 
 app.run(host="0.0.0.0", port=PORT)
